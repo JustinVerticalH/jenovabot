d2e403766c79a2a5f20e4bce185d200abc34e33b
JustinVerticalH <justinlevine123@gmail.com>
2022-08-08 19:25:22 -0400

    In progress

--- jenovabot.py
+++ jenovabot.py
@@ -20,6 +20,7 @@ async def on_message(message):
         await message.channel.send("Shut the fuck up you maggot. You clearly don't understand what makes a great video game. Elden Ring is a beautifully crafted masterpiece with a rich-open, beautiful graphics, fantastical gameplay, a great narrative, great quest design and it gives a ton of freedom and an actual challenge. Meanwhile all the other games that came out this year are overrated, mediocre games with boring, generic and repetitive gameplay, boring and uninteresting narratives and keep telling you what to do every 5 seconds. You and the people that support these kinds of doghit games are everything that is wrong this the gaming industry. These companies give you garbage and you guys eat it up and ask for more. Elden Ring is literally the only game that deserves to be called a true video game. Everything else is a joke and a scam. So fuck you, fuck all the people that pay for it, and fuck these companies that keep pumping these shitty mediocre kiddy games. I hope all of you fuckers die. Elden Ring and FromSoftware deserve all the praise and much more. They are single-handedly carrying the entire gaming industry with their state of the art games.")
     await bot.process_commands(message)
 
+# Not working correctly yet. Fix this
 @bot.event
 async def on_scheduled_event_create(event):
     print("Made it here")
