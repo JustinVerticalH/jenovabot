cc0f0003ad445b92a49a9f50efc5ca21c24cf0e7
JustinVerticalH <justinlevine123@gmail.com>
2022-08-09 20:56:11 -0400

    Event ping adjusts itself for timezones

--- jenovabot.py
+++ jenovabot.py
@@ -22,7 +22,6 @@ async def on_message(message: discord.Message):
         await message.channel.send("Shut the fuck up you maggot. You clearly don't understand what makes a great video game. Elden Ring is a beautifully crafted masterpiece with a rich-open, beautiful graphics, fantastical gameplay, a great narrative, great quest design and it gives a ton of freedom and an actual challenge. Meanwhile all the other games that came out this year are overrated, mediocre games with boring, generic and repetitive gameplay, boring and uninteresting narratives and keep telling you what to do every 5 seconds. You and the people that support these kinds of doghit games are everything that is wrong this the gaming industry. These companies give you garbage and you guys eat it up and ask for more. Elden Ring is literally the only game that deserves to be called a true video game. Everything else is a joke and a scam. So fuck you, fuck all the people that pay for it, and fuck these companies that keep pumping these shitty mediocre kiddy games. I hope all of you fuckers die. Elden Ring and FromSoftware deserve all the praise and much more. They are single-handedly carrying the entire gaming industry with their state of the art games.")
     await bot.process_commands(message)
 
-# Not working correctly yet. Fix this
 @bot.event
 async def on_scheduled_event_create(event: discord.ScheduledEvent):
     for role in event.guild.roles:
@@ -31,7 +30,6 @@ async def on_scheduled_event_create(event: discord.ScheduledEvent):
             start_time = int(event.start_time.timestamp())
             await channel.send(f"{event.name} is set for <t:{start_time}>! {role.mention}")
 
-# Test this. Does the channel preference persist after the bot stops running?
 @bot.command()
 async def alerts(ctx, arg: str):
     for channel in ctx.guild.channels:
