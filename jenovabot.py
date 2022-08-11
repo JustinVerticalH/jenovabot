236c1761472405f6a4cb9355d875fea6adb93718
Noah Levitt <noahhlevitt@gmail.com>
2022-08-11 15:10:22 -0400

    Improved "streampause" command

--- jenovabot.py
+++ jenovabot.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import discord
 from discord.ext import commands
 import json
@@ -9,6 +10,8 @@ token = os.getenv("TOKEN")
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+streampause_data: dict[discord.Message, discord.Member] = None
 
 def read(key: str) -> any:
     with open("values.json", "r+") as values_file:
@@ -42,13 +45,35 @@ async def on_scheduled_event_create(event: discord.ScheduledEvent):
 
 @bot.event
 async def on_reaction_add(reaction: discord.Reaction, user: discord.Member):
-    if not user.bot and reaction.message.id == read("streampause_message_id"):
-        if reaction.count == len(user.voice.channel.members) + 1:
-            original_author = await user.guild.fetch_member(read("streampause_author_id"))
-            await reaction.message.channel.send(f"{original_author.mention} Everyone's here!")
+    global streampause_data
+    if streampause_data is not None:
+        await attempt_to_finish_streampause(reaction, user, user.voice.channel if user.voice else None)
 
-            write("streampause_message_id", None)
-            write("streampause_author_id", None)
+@bot.event
+async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+    global streampause_data
+    if streampause_data is not None:
+        voice_channel = before.channel if after.channel is None else after.channel if before.channel is None else None
+        reaction = next(reaction for reaction in streampause_data["message"].reactions if reaction.emoji == "👍")
+
+        await attempt_to_finish_streampause(reaction, member, voice_channel)
+
+async def attempt_to_finish_streampause(reaction: discord.Reaction, user: discord.Member, voice_channel: Optional[discord.VoiceChannel]):
+    global streampause_data
+    if user.bot or reaction.message != streampause_data["message"] or reaction.emoji != "👍" or voice_channel is None:
+        return
+
+    reacted_members = set(await reaction.users().flatten())
+    vc_members = set(voice_channel.members)
+
+    if reacted_members & vc_members == vc_members:
+        original_author = streampause_data["author"]
+        ping_message = await reaction.message.channel.send(f"{original_author.mention} Everyone's here!")
+
+        await reaction.message.delete()
+        await ping_message.delete(delay=5.0)
+
+        streampause_data = None
 
 @bot.command()
 async def alerts(context: commands.Context, argument: str):
@@ -61,10 +86,21 @@ async def alerts(context: commands.Context, argument: str):
 
 @bot.command()
 async def streampause(context: commands.Context):
+    await context.message.delete(delay=2.0)
+    if context.author.voice is None:
+        message = await context.send("This command is only usable inside a voice channel.")
+        await message.delete(delay=5.0)
+        return
+
     message = await context.send("React with 👍 when you're all set!")
+
+    global streampause_data
+    streampause_data = {
+        "message": message,
+        "author": context.author
+    }
+
     await message.add_reaction("👍")
-    write("streampause_message_id", message.id)
-    write("streampause_author_id", context.author.id)
 
 @bot.command()
 async def test(context: commands.Context):
