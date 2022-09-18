bddddeecaf9a566381169df1cb293f38818e6ae5
JustinVerticalH <justinlevine123@gmail.com>
2022-09-18 03:19:19 -0400

    Alerts now pings stream role if event creator joins voice channel within 30 minutes of event start time

--- cogfiles/alerts.py
+++ cogfiles/alerts.py
@@ -1,6 +1,6 @@
 from ioutils import read_sql, write_sql
 
-import discord
+import discord, datetime
 from discord.ext import commands
 
 
@@ -9,7 +9,7 @@ class EventAlerts(commands.Cog, name="Event Alerts"):
     
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
     async def on_scheduled_event_create(self, event: discord.ScheduledEvent):
         """Send a ping message when an event tied to a role is created."""
@@ -20,6 +20,18 @@ class EventAlerts(commands.Cog, name="Event Alerts"):
                 start_time = int(event.start_time.timestamp())
                 await channel.send(f"{event.name} is set for <t:{start_time}>! {role.mention}")
     
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+        if before.channel is None and after.channel is not None: # Member has joined a voice channel
+            events = await member.guild.fetch_scheduled_events()
+            for event in events:
+                if event.creator.id == member.id:
+                    role = EventAlerts.get_role_from_event(event)
+                    time_until_event_start = event.start_time - datetime.datetime.now().astimezone(event.start_time.tzinfo)
+                    if time_until_event_start <= datetime.timedelta(minutes = 30):
+                        channel = await event.guild.fetch_channel(read_sql("test_settings", event.guild.id, "scheduled_event_alert_channel_id"))
+                        await channel.send(f"{event.name} is starting soon! {role.mention}")
+
     @commands.command()
     @commands.has_guild_permissions(manage_guild=True)
     async def alerts(self, context: commands.Context, channel: discord.TextChannel):
@@ -34,3 +46,10 @@ class EventAlerts(commands.Cog, name="Event Alerts"):
             await context.send("User needs Manage Server permission to use this command.")
         elif isinstance(error, commands.errors.ChannelNotFound):
             await context.send("Channel not found. Try again.")
+
+    @staticmethod
+    def get_role_from_event(event: discord.ScheduledEvent) -> discord.Role:
+        for role in event.guild.roles:
+            if "Ping" in role.name and role.name.replace(" Ping", "") in event.name:
+                return role
+        return None
