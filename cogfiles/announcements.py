169e3af104971c36b845b78013e05e8e2955d9ce
pokedart9001 <noahhlevitt@gmail.com>
2022-10-27 17:01:14 -0400

    cleaner periodic announcement loops

--- cogfiles/announcements.py
+++ cogfiles/announcements.py
@@ -10,13 +10,13 @@ class Announcements(commands.Cog, name="Periodic Announcements"):
     
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        
 
     @commands.Cog.listener()
     async def on_ready(self):
         """Start the periodic announcements processing loop."""
 
-        self.periodic_announcements.start()
+        self.ninja_troll.start()
+        self.first_of_the_month.start()
 
     @commands.command()
     @commands.has_guild_permissions(manage_guild=True)
@@ -25,7 +25,7 @@ class Announcements(commands.Cog, name="Periodic Announcements"):
         
         write_sql(DATABASE_SETTINGS, context.guild.id, "periodic_announcement_channel_id", channel.id)
         await context.send(f"Periodic announcement channel is set to {channel.mention}")
-    
+
     @announcements.error
     async def permissions_or_channel_fail(self, context: commands.Context, error: commands.errors.CommandError):
         if isinstance(error, commands.errors.MissingPermissions):
@@ -33,16 +33,28 @@ class Announcements(commands.Cog, name="Periodic Announcements"):
         elif isinstance(error, commands.errors.ChannelNotFound):
             await context.send("Channel not found. Try again.")
 
-    @tasks.loop(seconds=12)
-    async def periodic_announcements(self):
+    @tasks.loop(time=datetime.time(hour=17, tzinfo=pytz.timezone("US/Eastern"))) # 5:00 PM EST
+    async def ninja_troll(self):
         """Send periodic announcement messages at their appropriate times."""
         
-        now = datetime.datetime.now(pytz.timezone("US/Eastern"))
+        if datetime.date.today().weekday() != 4: # Friday
+            return
+        
         for guild in self.bot.guilds:
             channel_id = read_sql(DATABASE_SETTINGS, guild.id, "periodic_announcement_channel_id")
             if channel_id is not None:
-                channel = self.bot.get_channel(channel_id)
-                if now.weekday() == 4 and now.hour == 17 and now.minute == 0 and now.second < 12: # Friday, 5:00 PM EST
-                    await channel.send(file=discord.File("ninja_troll.png"))
-                if now.day == 1 and now.hour == 0 and now.minute == 0 and now.second < 12: # 1st day of the month, 12:00 AM EST
-                    await channel.send(file=discord.File("first_of_the_month.mov"))
+                channel = await self.bot.fetch_channel(channel_id)
+                await channel.send(file=discord.File("ninja_troll.png"))
+
+    @tasks.loop(time=datetime.time(hour=0, tzinfo=pytz.timezone("US/Eastern"))) # 12:00 AM EST
+    async def first_of_the_month(self):
+        """Send periodic announcement messages at their appropriate times."""
+        
+        if datetime.date.today().day != 1: # 1st day of the month
+            return
+        
+        for guild in self.bot.guilds:
+            channel_id = read_sql(DATABASE_SETTINGS, guild.id, "periodic_announcement_channel_id")
+            if channel_id is not None:
+                channel = await self.bot.fetch_channel(channel_id)
+                await channel.send(file=discord.File("first_of_the_month.mov"))
