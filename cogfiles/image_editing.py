649373ff05382e7bd45a9fba87371f8d89ce11e9
Justin Levine <justinlevine123@gmail.com>
2022-12-23 16:28:26 -0500

    Added image editing cog, image templates, and font

---
+++ cogfiles/image_editing.py
@@ -0,0 +1,59 @@
+import discord, os, random, tempfile, textwrap
+from discord.ext import commands
+from PIL import Image, ImageDraw, ImageFont
+
+class ImageEditing(commands.Cog, name="Image Editing"):
+    """Place text on various image templates."""
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.group(aliases=["kagetsu", "kt", "typemoondaily"], invoke_without_command=True)    
+    async def kagetsutoya(self, context: commands.Context, *, text: str):
+        templates = [x for x in os.listdir("image_resources") if x.find(".png") >= 0]
+        template = (f"image_resources/{random.choice(templates)}")
+
+        await ImageEditing.send_kagetsutoya(context, template, text)
+
+    @kagetsutoya.group()
+    async def akiha(self, context: commands.Context, *, text: str):
+        await ImageEditing.send_kagetsutoya(context, "image_resources/akiha.png", text)
+
+    @kagetsutoya.group(aliases=["arc"])
+    async def arcueid(self, context: commands.Context, *, text: str):
+        await ImageEditing.send_kagetsutoya(context, "image_resources/arcueid.png", text)
+    
+    @kagetsutoya.group()
+    async def ciel(self, context: commands.Context, *, text: str):
+        await ImageEditing.send_kagetsutoya(context, "image_resources/ciel.png", text)
+    
+    @kagetsutoya.group()
+    async def hisui(self, context: commands.Context, *, text: str):
+        await ImageEditing.send_kagetsutoya(context, "image_resources/hisui.png", text)
+    
+    @kagetsutoya.group()
+    async def kohaku(self, context: commands.Context, *, text: str):
+        await ImageEditing.send_kagetsutoya(context, "image_resources/kohaku.png", text)
+
+    @kagetsutoya.group()
+    async def satsuki(self, context: commands.Context, *, text: str):
+        await ImageEditing.send_kagetsutoya(context, "image_resources/satsuki.png", text)
+
+    @staticmethod
+    async def send_kagetsutoya(context: commands.Context, template_name: str, text: str):
+        """Add text to the provided image template and send the image. This function uses the Sazanami Gothic font."""
+        with Image.open(template_name) as image:
+            draw = ImageDraw.Draw(image)
+            draw.font = ImageFont.truetype("image_resources/sazanami-gothic.ttf", 28)
+            
+            # If the user doesn't provide line breaks, then format the text so that it 
+            if "\n" not in text:
+                text = textwrap.fill(text, width=44)
+            draw.text((145, 120), text, fill=(255, 255, 255))
+            
+            fp = tempfile.NamedTemporaryFile(suffix=".png", delete=False) 
+            fp.close()
+            image.save(fp.name)
+            f = discord.File(fp.name, filename="edit.png")
+            await context.send(file=f)
+            os.unlink(fp.name)
