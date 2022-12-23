f84e2edc8bd9ec23a5dfbc0128e6bc90da291126
Justin Levine <justinlevine123@gmail.com>
2022-12-23 16:29:57 -0500

    Displays who has and hasn't reacted to message

--- cogfiles/streampause.py
+++ cogfiles/streampause.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from ioutils import RandomColorEmbed
 
 import discord
 from discord.ext import commands
@@ -19,12 +20,22 @@ class StreamPause(commands.Cog, name="Stream Pause"):
             await self.attempt_to_finish_streampause(reaction, user, user.voice.channel if user.voice else None)
 
     @commands.Cog.listener()
+    async def on_reaction_remove(self, reaction: discord.Reaction, user: discord.Member):
+        """Keep track of reactions added to a streampause message, if there is one."""
+
+        if self.streampause_data is not None:
+            await self.attempt_to_finish_streampause(reaction, user, user.voice.channel if user.voice else None)
+
+    @commands.Cog.listener()
     async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
         """Keep track of members entering or leaving a voice channel during a streampause, if there is one."""
         
         if self.streampause_data is not None:
             voice_channel = before.channel if after.channel is None else after.channel if before.channel is None else None
-            reaction = discord.utils.get(self.streampause_data["message"].reactions, emoji="👍")
+
+            # Fetching the message again to get the most updated state of the message
+            message = await self.streampause_data["message"].fetch()
+            reaction = discord.utils.get(message.reactions, emoji="👍")
 
             await self.attempt_to_finish_streampause(reaction, member, voice_channel)
 
@@ -36,7 +47,11 @@ class StreamPause(commands.Cog, name="Stream Pause"):
             await context.send("This command is only usable inside a voice channel.")
             return
 
-        message = await context.send("React with 👍 when you're all set!")
+        embed = RandomColorEmbed(
+            title = "React with 👍 when you're all set!"
+        )
+        message = await context.send(embed=embed)
+
 
         self.streampause_data = {
             "message": message,
@@ -44,12 +59,16 @@ class StreamPause(commands.Cog, name="Stream Pause"):
         }
 
         await message.add_reaction("👍")
+        await message.pin()
+        await self.update_message(message, context.author.voice.channel)
 
     async def attempt_to_finish_streampause(self, reaction: discord.Reaction, user: discord.Member, voice_channel: Optional[discord.VoiceChannel]):
         """Attempt to end a streampause upon a change to either reactions or voice channel members."""
         
-        if user == self.bot or reaction.message != self.streampause_data["message"] or reaction.emoji != "👍" or voice_channel is None:
+        if user.bot or reaction.message != self.streampause_data["message"] or reaction.emoji != "👍" or voice_channel is None:
             return
+
+        await self.update_message(reaction.message, voice_channel)
 
         reacted_members = {user async for user in reaction.users()}
         vc_members = set(voice_channel.members)
@@ -60,3 +79,24 @@ class StreamPause(commands.Cog, name="Stream Pause"):
 
             await reaction.message.delete()
             self.streampause_data = None
+
+    async def update_message(self, message: discord.Message, voice_channel: Optional[discord.VoiceChannel]):
+        if voice_channel is not None:
+            members = voice_channel.members
+
+            reacted_list = []
+            if message.reactions:
+                reacted_list = [user async for user in message.reactions[0].users()]
+
+            reacted_members = "**Reacted:**"
+            not_reacted_members = "**Not Reacted:**"
+
+            for member in members:
+                if not member.bot:
+                    if member in reacted_list:
+                        reacted_members += f"\n{member.name}"
+                    else:
+                        not_reacted_members += f"\n{member.name}"
+        
+            embed = RandomColorEmbed(title=message.embeds[0].title, colour=message.embeds[0].colour, description=f"{reacted_members}\n\n{not_reacted_members}")
+            await message.edit(embed=embed)
