da433a5c76d1d742c29b959fc41584308ceeba47
Justin Levine <96055353+JustinVerticalH@users.noreply.github.com>
2024-11-25 22:39:35 -0500

    Added a listener that responds to forum threads and posts a message about the new thread (#9)

--- cogfiles/admin.py
+++ cogfiles/admin.py
@@ -3,7 +3,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from enum import Enum
-from ioutils import write_json
+from ioutils import read_json, write_json
 
 
 class ChannelType(Enum):
@@ -11,12 +11,14 @@ class ChannelType(Enum):
     Birthdays = "Birthdays"
     DailyMessages = "Daily messages"
     Events = "Events"
+    NewThreads = "New thread announcements"
 
 CHANNEL_TYPE_TO_DATABASE_STR = {
     ChannelType.Announcements: "periodic_announcement_channel_id",
     ChannelType.Birthdays: "birthday_channel_id",
     ChannelType.DailyMessages: "daily_message_channel_id",
-    ChannelType.Events: "scheduled_event_alert_channel_id"
+    ChannelType.Events: "scheduled_event_alert_channel_id",
+    ChannelType.NewThreads: "new_threads_channel_id"
 }
 
 class Admin(commands.Cog, name="Administrator"):
@@ -52,11 +54,11 @@ class Admin(commands.Cog, name="Administrator"):
             await interaction.response.send_message(f"Failed to sync command(s): {error}", ephemeral=True)
         else:
             await interaction.response.send_message(f"Synced {len(synced)} command(s).", ephemeral=True)
-    
+
     @app_commands.command()
     @app_commands.checks.has_permissions(manage_guild=True)
     async def channel(self, interaction: discord.Interaction, channel_type: ChannelType, channel: discord.TextChannel | discord.ForumChannel):
-        """Set which channel to send certain automated messages (announcements, birthdays, daily messages, events, etc)."""
+        """Set which channel to send certain automated messages (announcements, birthdays, daily messages, events, new threads, etc)."""
         if channel_type != ChannelType.Events and isinstance(channel, discord.ForumChannel):
             return await interaction.response.send_message("Forum channels are only possible when setting the Events channel.", ephemeral=True)
         write_json(interaction.guild.id, CHANNEL_TYPE_TO_DATABASE_STR[channel_type], value=channel.id)
@@ -67,3 +69,16 @@ class Admin(commands.Cog, name="Administrator"):
         """Handles errors for the given command (insufficient permissions, etc)."""
         if isinstance(error, app_commands.errors.MissingPermissions):
             await interaction.response.send_message("You need the Manage Server permission to use this command.", ephemeral=True)
+
+    @commands.Cog.listener()
+    async def on_thread_create(self, thread: discord.Thread):
+        """Sends a message informing users of a new thread."""
+        if not isinstance(thread.parent, discord.ForumChannel) or thread.is_private():
+            return
+
+        new_thread_channel_id = read_json(thread.guild.id, "new_threads_channel_id")
+        if new_thread_channel_id is None:
+            return
+
+        new_thread_channel = await self.bot.fetch_channel(new_thread_channel_id)
+        await new_thread_channel.send(f"{thread.owner.mention} has created a new thread in {thread.parent.mention}: {thread.mention}")
