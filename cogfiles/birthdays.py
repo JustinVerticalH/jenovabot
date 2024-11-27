744e89ed4874ea4171a7a29272151689270c7292
JustinVerticalH <justinlevine123@gmail.com>
2024-11-27 15:44:46 -0500

    Standardized all the to_json and from_json calls

--- cogfiles/birthdays.py
+++ cogfiles/birthdays.py
@@ -1,5 +1,6 @@
 import datetime, zoneinfo
-from ioutils import read_json, write_json, RandomColorEmbed
+from dataclasses import dataclass, field
+from ioutils import  JsonSerializable, RandomColorEmbed, read_json, write_json, initialize_from_json
 
 import discord
 from discord import app_commands
@@ -21,23 +22,36 @@ class Month(Enum):
     November = 11
     December = 12
 
+@dataclass(frozen=True, order=True)
+class Birthday(JsonSerializable):
+    user: discord.Member
+    date: datetime.date = field(compare=False)
+
+    def to_json(self) -> dict[str, int | str]:
+        """Convert the current ReactionRole object to a JSON string."""
+        return {
+            "user_id": self.user.id,
+            "date": datetime.datetime.strftime(self.date,"%Y-%m-%d")
+        }
+
+    @staticmethod
+    async def from_json(bot: commands.Bot, json_obj: dict[str, int | str]):
+        """Convert a JSON dictionary to a ReactionRole object."""
+        user = await bot.fetch_user(int(json_obj["user_id"]))
+        date = datetime.datetime.strptime(json_obj["date"], "%Y-%m-%d").date()
+        return Birthday(user, date)
+
 class Birthdays(commands.Cog, name="Birthdays"):
     "Send messages on members' birthdays."
     
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.birthdays: dict[int, dict[discord.User, datetime.date]] = {}
+        self.birthdays: dict[int, set[Birthday]] = {}
 
     @commands.Cog.listener()
     async def on_ready(self):
         """Initialize the list of birthdays and sync application commands."""
-        for guild in self.bot.guilds:
-            guild_birthdays = read_json(guild.id, "birthdays")
-            if guild_birthdays is None:
-                write_json(guild.id, "birthdays", value={})
-                self.birthdays[guild.id] = {}
-            else:
-                self.birthdays[guild.id] = {await self.bot.fetch_user(user_id): datetime.datetime.strptime(birthday, "%Y-%m-%d").date() for user_id, birthday in guild_birthdays.items()}
+        await initialize_from_json(self.bot, self.birthdays, Birthday, "birthdays")
 
         self.send_birthday_message.start()
 
@@ -55,8 +69,12 @@ class Birthdays(commands.Cog, name="Birthdays"):
             return await interaction.response.send_message("Invalid date.", ephemeral=True)
         if self.birthdays[interaction.guild.id] is None:
             self.birthdays[interaction.guild.id] = {}
-        self.birthdays[interaction.guild.id][interaction.user] = date
-        write_json(interaction.guild.id, "birthdays", value={user.id: birthday.isoformat() for user, birthday in self.birthdays[interaction.guild.id].items()})
+
+        birthday = Birthday(interaction.user, date)
+        old_birthday = next((b for b in self.birthdays[interaction.guild.id] if b.user == birthday.user), None)
+        self.birthdays[interaction.guild.id].discard(old_birthday)
+        self.birthdays[interaction.guild.id].add(birthday)
+        write_json(interaction.guild.id, "birthdays", value=[birthday.to_json() for birthday in self.birthdays[interaction.guild.id]])
         await interaction.response.send_message(f"Added your birthday: {month.name} {ordinal(day)}{'' if year == None else f', {year}'}", ephemeral=True)
 
     @app_commands.command()
@@ -67,15 +85,15 @@ class Birthdays(commands.Cog, name="Birthdays"):
         # Add the dates that have already happened after the dates that haven't
         # This gives a list of upcoming birthdays in sorted order, including some dates from next year after this year
         now = datetime.datetime.now().date()
-        next_birthdays = {user: birthday.replace(year=now.year) for user, birthday in self.birthdays[interaction.guild.id].items()}
-        sorted_birthdays = {user: birthday for user, birthday in sorted(next_birthdays.items(), key=lambda item: item[1])}
-        sorted_birthdays = {user: birthday for user, birthday in sorted_birthdays.items() if now < birthday} | {user: birthday.replace(year=now.year+1) for user, birthday in sorted_birthdays.items() if now >= birthday}
+        next_birthdays = {Birthday(birthday.user, birthday.date.replace(year=now.year)) for birthday in self.birthdays[interaction.guild.id]}        
+        sorted_birthdays = sorted(next_birthdays, key=lambda birthday: birthday)
+        sorted_birthdays = {birthday for birthday in sorted_birthdays if now < birthday.date} | {Birthday(birthday.user, birthday.date.replace(year=now.year+1)) for birthday in sorted_birthdays if now >= birthday.date}
 
         description = ""
-        n = min(10, len(sorted_birthdays.values()))
-        for user, birthday in list(sorted_birthdays.items())[:n]:
-            birthday_str = f"{Month(birthday.month).name} {ordinal(birthday.day)}, {birthday.year}"
-            description += f"**{birthday_str}**\n{user.mention}\n\n"
+        n = min(10, len(sorted_birthdays))
+        for birthday in list(sorted_birthdays)[:n]:
+            birthday_str = f"{Month(birthday.date.month).name} {ordinal(birthday.date.day)}, {birthday.date.year}"
+            description += f"**{birthday_str}**\n{birthday.user.mention}\n\n"
 
         embed = RandomColorEmbed(title="Upcoming Birthdays", description=description)
         await interaction.response.send_message(embed=embed, ephemeral=True)
@@ -88,17 +106,17 @@ class Birthdays(commands.Cog, name="Birthdays"):
             if channel_id is None:
                 continue
             
-            for user, birthday in self.birthdays[guild.id].items():
+            for birthday in self.birthdays[guild.id]:
                 now = datetime.datetime.now(tz=zoneinfo.ZoneInfo("US/Eastern"))
-                if birthday.month == now.month and birthday.day == now.day:
+                if birthday.date.month == now.month and birthday.date.day == now.day:
                     # Send birthday message in the correct channel
                     channel = await self.bot.fetch_channel(channel_id)
                     
-                    if birthday.year == datetime.MINYEAR:
-                        await channel.send(f"Happy birthday {user.mention}!")
+                    if birthday.date.year == datetime.MINYEAR:
+                        await channel.send(f"Happy birthday {birthday.user.mention}!")
                     else:
-                        age = now.year - birthday.year
-                        await channel.send(f"Happy {ordinal(age)} birthday {user.mention}!")
+                        age = now.year - birthday.date.year
+                        await channel.send(f"Happy {ordinal(age)} birthday {birthday.user.mention}!")
 
 
 def ordinal(n: int) -> str:
--- cogfiles/reaction_roles.py
+++ cogfiles/reaction_roles.py
@@ -1,36 +1,36 @@
-import discord, json
+import discord, re
 from dataclasses import dataclass, field
 from discord import app_commands
 from discord.ext import commands
-from ioutils import read_json, write_json
+from ioutils import JsonSerializable, RandomColorEmbed, write_json, initialize_from_json
 
 
 @dataclass(frozen=True, order=True)
-class ReactionRole:
+class ReactionRole(JsonSerializable):
     """Data associated with a reaction role."""
     channel: discord.TextChannel = field(compare=False)
-    message: discord.Message = field()
+    message: discord.Message = field(compare=False)
     role: discord.Role = field()
-    emoji: str = field()
+    emoji: discord.PartialEmoji = field(compare=False)
 
     def to_json(self) -> dict[str, int | str]:
-        """Convert the current reminder object to a JSON string."""
+        """Convert the current ReactionRole object to a JSON string."""
         return {
             "message_id": self.message.id,
             "channel_id": self.channel.id,
             "role_id": self.role.id,
-            "emoji": self.emoji
+            "emoji": str(self.emoji)
         }
 
     @staticmethod
     async def from_json(bot: commands.Bot, json_obj: dict[str, int | str]):
-        """Convert a JSON dictionary to a Reminder object."""
+        """Convert a JSON dictionary to a ReactionRole object."""
         channel = await bot.fetch_channel(json_obj["channel_id"])
         
         if channel is not None:
             message = await channel.fetch_message(json_obj["message_id"])
             role = channel.guild.get_role(json_obj["role_id"])
-            emoji = json_obj["emoji"]
+            emoji = discord.PartialEmoji.from_str(json_obj["emoji"])
             return ReactionRole(channel, message, role, emoji)
 
 class ReactionRoles(commands.Cog, name="Reaction Roles"):
@@ -42,30 +42,41 @@ class ReactionRoles(commands.Cog, name="Reaction Roles"):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        for guild in self.bot.guilds:
-            if read_json(guild.id, "reaction_roles") is None:
-                write_json(guild.id, "reaction_roles", value={})
-            try:
-                self.reactionroles[guild.id] = {await ReactionRole.from_json(self.bot, json_str) for json_str in read_json(guild.id, "reaction_roles")}
-            except json.JSONDecodeError as e:
-                print(e)
+        """Initialize the list of reaction roles in memory."""
+        await initialize_from_json(self.bot, self.reactionroles, ReactionRole, "reaction_roles")
     
     @app_commands.command()
     @app_commands.rename(emoji_str="emoji")
     @app_commands.checks.has_permissions(manage_guild=True)
-    async def reactionrole(self, interaction: discord.Interaction, role: discord.Role, emoji_str: str):
+    async def reactionrole(self, interaction: discord.Interaction, role: discord.Role, emoji_str: str, message_link: str):
         "Set up a reaction role message."
-        #emoji = discord.PartialEmoji.from_str(emoji_str)
-        emoji = emoji_str
+        emoji = discord.PartialEmoji.from_str(emoji_str)
 
-        await interaction.response.defer()
+        regex_searches = re.search("(\d+)\/(\d+)$", message_link)
+        try:
+            channel_id = regex_searches.group(1)
+            message_id = regex_searches.group(2)
+            channel = await interaction.guild.fetch_channel(int(channel_id))
+            message = await channel.fetch_message(int(message_id))
+        except:
+            return await interaction.response.send_message("Could not find the given message.", ephemeral=True)
 
-        message = await interaction.followup.send(f"React here for the {role.name} role:\n> {emoji} {role.name}")
-        await message.add_reaction(emoji)
+        try:
+            await message.add_reaction(emoji)
+        except discord.errors.HTTPException as e:
+            return await interaction.response.send_message("Could not use the given emoji. Make sure that this bot shares a server with the emoji.", ephemeral=True)
 
-        reactionrole = ReactionRole(interaction.channel, message, role, emoji)
+        reactionrole = ReactionRole(channel, message, role, emoji)
+
+        # If there already exists a reaction role for this role, remove it and replace it with the new one
+        old_reactionrole = next(rr for rr in self.reactionroles[interaction.guild.id] if rr.role == reactionrole.role)
+        self.reactionroles[interaction.guild.id].discard(old_reactionrole)
         self.reactionroles[interaction.guild.id].add(reactionrole)
         write_json(interaction.guild.id, "reaction_roles", value=[reactionrole.to_json() for reactionrole in self.reactionroles[interaction.guild.id]])
+        embed = RandomColorEmbed(title="Reaction Role", description=f"React to this message with {emoji} to get the {role.mention} role: {message.jump_url}")
+        if old_reactionrole is not None:
+            embed.description += f"\nThis command replaced an existing reaction role for {role.mention} at {old_reactionrole.message.jump_url}"
+        await interaction.response.send_message(embed=embed, ephemeral=True)
     
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
@@ -73,10 +84,7 @@ class ReactionRoles(commands.Cog, name="Reaction Roles"):
         This is called regardless of the state of the internal message cache, for example with old messages."""
         guild = self.bot.get_guild(payload.guild_id)
         member = guild.get_member(payload.user_id)
-        if member.bot or all(reactionrole.message.id != payload.message_id for reactionrole in self.reactionroles[payload.guild_id]):
-            return
-        
-        role = next(reactionrole.role for reactionrole in self.reactionroles[payload.guild_id] if reactionrole.emoji == str(payload.emoji))
+        role = self.get_role(payload)
 
         await member.add_roles(role)
 
@@ -90,13 +98,19 @@ class ReactionRoles(commands.Cog, name="Reaction Roles"):
         This is called regardless of the state of the internal message cache, for example with old messages."""
         guild = self.bot.get_guild(payload.guild_id)
         member = guild.get_member(payload.user_id)
-        if member.bot or all(reactionrole.message.id != payload.message_id for reactionrole in self.reactionroles[payload.guild_id]):
-            return
-        
-        role = next(reactionrole.role for reactionrole in self.reactionroles[payload.guild_id] if reactionrole.emoji == str(payload.emoji))
+        role = self.get_role(payload)
 
         await member.remove_roles(role)
 
         if member.dm_channel is None:
             await member.create_dm()
         await member.dm_channel.send(f"You no longer have the {role.name} role in {guild.name}.")
+    
+    def get_role(self, payload: discord.RawReactionActionEvent) -> discord.Role | None:
+        """Gets the role that should be given/removed to the user on a given reaction.
+        Returns None if the reaction does not correspond to any reaction role."""
+        guild = self.bot.get_guild(payload.guild_id)
+        member = guild.get_member(payload.user_id)
+        if member.bot or all(reactionrole.message.id != payload.message_id for reactionrole in self.reactionroles[payload.guild_id]):
+            return None
+        return next(reactionrole.role for reactionrole in self.reactionroles[payload.guild_id] if reactionrole.emoji == payload.emoji)
