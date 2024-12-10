b77ccecc7c2078f9a802899f2b4443db47717e16
JustinVerticalH <justinlevine123@gmail.com>
2024-12-10 18:55:26 -0500

    Reminder subscribe views now persist between restarts, and clicking reminder subscribe button now removes user from subscribers

--- cogfiles/reminders.py
+++ cogfiles/reminders.py
@@ -17,13 +17,12 @@ class Reminder(JsonSerializable):
     original_message_datetime: datetime.datetime = field(compare=False)
     reminder_datetime: datetime.datetime
     reminder_str: str = field(compare=False)
+    slash_message: discord.Message = field(compare=False) # This will be None when the reminder is created via regular command
     subscribers: set[discord.User] = field(hash=False)
 
     def __str__(self):
-        if self.command_message is None:
-            return f"{self.author.mention} - {self.channel.jump_url} @ {format_dt(self.reminder_datetime, style='F')}: {self.reminder_str!r}"
-        else:
-            return f"{self.command_message.author.mention} - {self.command_message.jump_url} @ {format_dt(self.reminder_datetime, style='F')}: {self.reminder_str!r}"
+        message = self.slash_message if self.command_message is None else self.command_message
+        return f"{self.author.mention} - {message.jump_url} @ {format_dt(self.reminder_datetime, style='F')}: {self.reminder_str!r}"
 
     def __repr__(self):
         MAX_LENGTH = 100
@@ -44,6 +43,7 @@ class Reminder(JsonSerializable):
             "original_message_timestamp": -1 if self.original_message_datetime is None else self.original_message_datetime.timestamp(),
             "reminder_timestamp": self.reminder_datetime.timestamp(),
             "reminder_str": self.reminder_str,
+            "slash_message_id": -1 if self.slash_message is None else self.slash_message.id,
             "subscriber_ids": [subscriber.id for subscriber in self.subscribers]
         }
 
@@ -59,27 +59,34 @@ class Reminder(JsonSerializable):
             reminder_str = json_obj["reminder_str"]
             author = await bot.fetch_user(json_obj["author_id"]) if command_message is None else command_message.author
             channel = await bot.fetch_channel(json_obj["channel_id"]) if command_message is None else command_message.channel
+            slash_message = None if json_obj["slash_message_id"] < 0 else await channel.fetch_message(json_obj["slash_message_id"])
             subscribers = [await bot.fetch_user(subscriber_id) for subscriber_id in json_obj["subscriber_ids"]] if command_message is None else []
 
-            return Reminder(author, channel, command_message, original_message_datetime, reminder_datetime, reminder_str, subscribers)
+            return Reminder(author, channel, command_message, original_message_datetime, reminder_datetime, reminder_str, slash_message, subscribers)
 
     def copy(self):
         # We need to ensure that different Reminder objects do not point to the same subscribers list
         # This ensures that when the list of subscribers in one Reminder is modified, copies of that Reminder are not also modified (such as Reminders._cached_reminders)
-        return Reminder(self.author, self.channel, self.command_message, self.original_message_datetime, self.reminder_datetime, self.reminder_str, self.subscribers.copy())
+        return Reminder(self.author, self.channel, self.command_message, self.original_message_datetime, self.reminder_datetime, self.reminder_str, self.slash_message, self.subscribers.copy())
 
 class ReminderSubscribeView(discord.ui.View):
 
-    def __init__(self, reminder: Reminder):
-        super().__init__()
-        self.reminder = reminder
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.reminder: Reminder | None # This will be None after the reminder has been sent
 
-    @discord.ui.button(label="I want to be reminded too!", emoji="👍")
+    @discord.ui.button(label="I want to be reminded too!", emoji="👍", custom_id="reminder_subscribe_button")
     async def subscribe_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if interaction.user == self.reminder.author or interaction.user in self.reminder.subscribers:
+        if self.reminder is None:
+            return await interaction.response.send_message("This reminder has already been sent.", ephemeral=True)
+        if interaction.user == self.reminder.author:
             return await interaction.response.send_message("You are already set to be notified of this reminder.", ephemeral=True) #TODO: Make this a toggleable button
-        self.reminder.subscribers.append(interaction.user)
-        return await interaction.response.send_message(f"You will be reminded of this {format_dt(self.reminder.reminder_datetime, style='R')}", ephemeral=True)
+        if interaction.user in self.reminder.subscribers:
+            self.reminder.subscribers.remove(interaction.user)
+            return await interaction.response.send_message("You will no longer be reminded of this.", ephemeral=True)
+        else:
+            self.reminder.subscribers.append(interaction.user)
+            return await interaction.response.send_message(f"You will be reminded of this {format_dt(self.reminder.reminder_datetime, style='R')}.", ephemeral=True)
 
 class ReminderCancelSelect(discord.ui.Select):
 
@@ -103,7 +110,7 @@ class ReminderCancelSelect(discord.ui.Select):
 
 class ReminderCancelView(discord.ui.View):
     def __init__(self, interaction: discord.Interaction, reminders: set[Reminder]):
-        super().__init__()
+        super().__init__(timeout=None)
         self.member = interaction.user
         self.add_item(ReminderCancelSelect(interaction, reminders))
     
@@ -125,6 +132,12 @@ class Reminders(commands.Cog, name="Reminders"):
         await initialize_from_json(self.bot, self.reminders, Reminder, "reminders")
         for guild in self.bot.guilds:
             self._cached_reminders[guild.id] = deepcopy(self.reminders[guild.id])
+            for reminder in self.reminders[guild.id]:
+                if reminder.slash_message is not None:
+                    # This makes the views on existing reminders persistent between application restarts
+                    view = ReminderSubscribeView()
+                    view.reminder = reminder
+                    self.bot.add_view(view=view, message_id=reminder.slash_message.id)
         self.send_reminders.start()
         self.sync_json.start()
 
@@ -142,16 +155,18 @@ class Reminders(commands.Cog, name="Reminders"):
         reminder_datetime = datetime.datetime.now() + timedelta
         if reminder_datetime <= datetime.datetime.now():
             return await interaction.response.send_message("Reminders cannot be set for the past.", ephemeral=True)
-        reminder = await self.create_reminder(interaction, reminder_datetime, reminder_str)
 
         embed = RandomColorEmbed(title="Reminder")
-        embed.description = f"You set a reminder for {format_dt(reminder_datetime, style='f')}:\n{reminder_str}"
-        view = ReminderSubscribeView(reminder)
+        embed.description = f"You set a reminder for {format_dt(reminder_datetime, style='f')}:\n\"{reminder_str}\""
+        view = ReminderSubscribeView()
         await interaction.response.send_message(embed=embed, view=view)
+        reminder = await self.create_reminder(interaction, reminder_datetime, reminder_str)
+        view.reminder = reminder
 
     async def create_reminder(self, interaction: discord.Interaction, time: datetime.datetime, reminder_str: str):
         "Creates a new reminder and adds it to the list of reminders."
-        reminder = Reminder(interaction.user, interaction.channel, None, datetime.datetime.now(), time, reminder_str, [])
+        message = await interaction.original_response()
+        reminder = Reminder(interaction.user, interaction.channel, None, datetime.datetime.now(), time, reminder_str, message, [])
         if interaction.guild.id not in self.reminders:
             self.reminders[interaction.guild.id] = set()
         self.reminders[interaction.guild.id].add(reminder)
@@ -190,16 +205,22 @@ class Reminders(commands.Cog, name="Reminders"):
                 if reminder.reminder_datetime.timestamp() <= datetime.datetime.now().timestamp():
                     # Read the list of reactions to the message, and create a string to mention each user (besides the bot) who reacted
                     if reminder.command_message is None: # This reminder was created via slash command
-                        subscribers_mention = "\n" + " ".join(user.mention for user in reminder.subscribers if user != self.bot.user and user != reminder.author)
-                        await reminder.channel.send(f"**Reminder** from {format_dt(reminder.original_message_datetime, 'R')}\n\"{reminder.reminder_str}\"\n{reminder.author.mention}{subscribers_mention}")
-                    else:
+                        message = reminder.slash_message
+                        subscribers_mention = " ".join(user.mention for user in reminder.subscribers if user != self.bot.user and user != reminder.author)
+                    else: # This reminder was created via regular command
+                        message = reminder.command_message
+                        subscribers_mention = ""
                         for reaction in reminder.command_message.reactions:
                             if reaction.emoji == "👍":
                                 subscribers = [user async for user in reaction.users()]
-                                subscribers_mention = "\n" + " ".join(user.mention for user in subscribers if user != self.bot.user and user != reminder.command_message.author)
-                                await reminder.command_message.reply(f"{reminder.reminder_str} {subscribers_mention}")
+                                subscribers_mention = " ".join(user.mention for user in subscribers if user != self.bot.user and user != reminder.command_message.author)
+                    await message.reply(f"**Reminder** from {format_dt(reminder.original_message_datetime, 'R')}\n\"{reminder.reminder_str}\"\n{reminder.author.mention}{subscribers_mention}")
 
+                    # Remove the reminder from memory
                     self.reminders[guild.id].remove(reminder)
+                    view = ReminderSubscribeView()
+                    view.reminder = None
+                    self.bot.add_view(view=view, message_id=reminder.slash_message.id)
     
     @tasks.loop(seconds=0.3)
     async def sync_json(self):
