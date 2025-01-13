eb32465a59337c334e898bcf89e10f057f8f53c6
JustinVerticalH <justinlevine123@gmail.com>
2025-01-12 21:49:56 -0500

    Change get_channel to fetch_channel

--- cogfiles/reminders.py
+++ cogfiles/reminders.py
@@ -50,7 +50,7 @@ class Reminder(JsonSerializable):
     @staticmethod
     async def from_json(bot: commands.Bot, json_obj: dict[str, int | float | str]):
         """Convert a JSON dictionary to a Reminder object."""
-        channel = bot.get_channel(json_obj["channel_id"])
+        channel = await bot.fetch_channel(json_obj["channel_id"])
         
         if channel is not None:
             command_message = None if json_obj["command_message_id"] < 0 else await channel.fetch_message(json_obj["command_message_id"])
