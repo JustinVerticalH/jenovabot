f2172280d598f73c2b3636b68bba59f8a1dd851b
JustinVerticalH <justinlevine123@gmail.com>
2025-09-11 12:01:16 -0400

    Creating reaction role without a preexisting message

--- cogfiles/reaction_roles.py
+++ cogfiles/reaction_roles.py
@@ -51,18 +51,24 @@ class ReactionRoles(commands.GroupCog, name="reactionrole"):
     @app_commands.command()
     @app_commands.rename(emoji_str="emoji")
     @app_commands.checks.has_permissions(manage_guild=True)
-    async def add(self, interaction: discord.Interaction, role: discord.Role, emoji_str: str, message_link: str):
-        "Attaches a reaction role to a message. When a user reacts to the message with the given emoji, they will receive the role."
+    async def add(self, interaction: discord.Interaction, role: discord.Role, emoji_str: str, message_link: str | None):
+        """Attaches a reaction role to a message. If no message is provided, the bot sends its own message.
+        When a user reacts to the message with the given emoji, they will receive the role."""
         emoji = discord.PartialEmoji.from_str(emoji_str)
 
-        regex_searches = re.search("(\d+)\/(\d+)$", message_link)
-        try:
-            channel_id = regex_searches.group(1)
-            message_id = regex_searches.group(2)
-            channel = await interaction.guild.fetch_channel(int(channel_id))
-            message = await channel.fetch_message(int(message_id))
-        except (discord.errors.HTTPException, AttributeError):
-            return await interaction.response.send_message("Could not find the given message.", ephemeral=True)
+        if message_link is None:
+            channel = interaction.channel
+            message = await channel.send(f"React with {emoji_str} for the {role.mention} role!")
+            message.activity = None
+        else:
+            regex_searches = re.search("(\d+)\/(\d+)$", message_link)
+            try:
+                channel_id = regex_searches.group(1)
+                message_id = regex_searches.group(2)
+                channel = await interaction.guild.fetch_channel(int(channel_id))
+                message = await channel.fetch_message(int(message_id))
+            except (discord.errors.HTTPException, AttributeError):
+                return await interaction.response.send_message("Could not find the given message.", ephemeral=True)
 
         try:
             await message.add_reaction(emoji)
@@ -95,6 +101,16 @@ class ReactionRoles(commands.GroupCog, name="reactionrole"):
         write_json(interaction.guild.id, "reaction_roles", value=[reactionrole.to_json() for reactionrole in self.reactionroles[interaction.guild.id]])
 
         embed = RandomColorEmbed(title="Reaction Role Removed", description=f"\nThe reaction role for {role.mention} at {old_reactionrole.message.jump_url} has been removed.")
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
+    @app_commands.command()
+    async def list(self, interaction: discord.Interaction):
+        "Lists all reaction roles in this server."
+        if interaction.guild.id not in self.reactionroles or len(self.reactionroles[interaction.guild.id]) == 0:
+            return await interaction.response.send_message("There are no reaction roles in this server.", ephemeral=True)
+        
+        description = "\n".join(f"{reactionrole.role.mention} with {reactionrole.emoji} at {reactionrole.message.jump_url}" for reactionrole in sorted(self.reactionroles[interaction.guild.id], key=lambda rr: rr.role.name.lower()))
+        embed = RandomColorEmbed(title="Reaction Roles", description=description)
         await interaction.response.send_message(embed=embed, ephemeral=True)
     
     @add.error
